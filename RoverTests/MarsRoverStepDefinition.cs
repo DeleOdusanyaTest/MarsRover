00b9da7185b2c00284e40656b0fe2815856e5c60
DeleOdusanyaTest <70922980+DeleOdusanyaTest@users.noreply.github.com>
2020-09-08 02:11:13 +0100

    Updated Step definition
    Added the code to execute the tests

--- RoverTests/MarsRoverStepDefinition.cs
+++ RoverTests/MarsRoverStepDefinition.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Rovers;
 using TechTalk.SpecFlow;
 
 namespace RoverTests
@@ -6,28 +7,54 @@ namespace RoverTests
     [Binding]
     public class MarsRoverStepDefinition
     {
+        private readonly ScenarioContext _scenarioContext;
+        Rover myRover;
+
+        public MarsRoverStepDefinition(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
         [Given(@"Rover is initialized with following position")]
         public void GivenRoverIsInitializedWithFollowingPosition(Table table)
         {
-            ScenarioContext.Current.Pending();
+            var row = table.Rows[0];
+            var x = Convert.ToInt32(row["x"]);
+            var y = Convert.ToInt32(row["y"]);
+
+            Rover rover = new Rover(x, y);
+            myRover = rover;
         }
-        
+
+
         [When(@"Rover position is set to the following position")]
-        public void WhenRoverPositionIsSetToTheFollowingPosition(Table table)
+        public void WhenInitialPositionIsSetToTheFollowingPosition(Table table)
         {
-            ScenarioContext.Current.Pending();
+            var row = table.Rows[0];
+            var x = Convert.ToInt32(row["x"]);
+            var y = Convert.ToInt32(row["y"]);
+            var facing = Convert.ToInt32(row["facing"]);
+            myRover.SetPosition(x, y, facing);
         }
-        
+
+        [Then(@"Rover is at the following position")]
+        public void ThenRoverIsInitializedWithNoErrorsWithFollowingPosition(Table table)
+        {
+            var row = table.Rows[0];
+            var x = row["x"];
+            var y = row["y"];
+            var facing = row["facing"];
+            var position = x + "," + y + "," + facing;
+
+            Assert.AreEqual(position, myRover.GetPosition());
+        }
+
         [When(@"Rover is moved with following command")]
         public void WhenRoverIsMovedWithFollowingCommand(Table table)
         {
-            ScenarioContext.Current.Pending();
-        }
-        
-        [Then(@"Rover is at the following position")]
-        public void ThenRoverIsAtTheFollowingPosition(Table table)
-        {
-            ScenarioContext.Current.Pending();
+            var row = table.Rows[0];
+            var command = row["Command"];
+            myRover.Process(command);
         }
     }
 }
